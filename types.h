07e7f0488f93e0a94c098af6ffa13722cb2f9a6e
Athidathan <147528927+Athidathan@users.noreply.github.com>
2025-11-08 22:03:24 +0530

    Os project

--- types.h
+++ types.h
@@ -2,3 +2,11 @@ typedef unsigned int   uint;
 typedef unsigned short ushort;
 typedef unsigned char  uchar;
 typedef uint pde_t;
+
+// Structure to hold process information for user-space
+struct procinfo {
+  int pid;           // Process ID
+  int state;         // Process state (enum procstate value)
+  uint sz;           // Size of process memory (bytes)
+  char name[16];     // Process name
+};
